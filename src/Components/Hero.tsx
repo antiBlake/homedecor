21ea4d89fe61db0ee6872f91cd37fba926d0e205
Jamesalehandro <Jamesalehandro>
2022-10-12 11:40:01 +0100

    test push

---
+++ src/Components/Hero.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+
+const Hero = (props: Props) => {
+  return <div>Hero</div>;
+};
+
+export default Hero;
---
+++ src/utils/route.ts
@@ -0,0 +1,6 @@
+export class decorRoutes {
+  static home = '/';
+  static products = '/products';
+  static about = '/about';
+  static signup = '/signup';
+}
